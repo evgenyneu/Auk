fcfebc294f7f75bd9c8ac62b944af38f9289b51a
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-13 19:19:45 +1000

    Initial Commit

---
+++ GreatAuk/GreatAuk.h
@@ -0,0 +1,19 @@
+//
+//  GreatAuk.h
+//  GreatAuk
+//
+//  Created by Evgenii on 13/06/2015.
+//  Copyright (c) 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GreatAuk.
+FOUNDATION_EXPORT double GreatAukVersionNumber;
+
+//! Project version string for GreatAuk.
+FOUNDATION_EXPORT const unsigned char GreatAukVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GreatAuk/PublicHeader.h>
+
+
