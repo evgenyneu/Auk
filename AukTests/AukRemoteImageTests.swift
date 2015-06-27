8d4d72e911b9826bc9becbeeee569bd3872ac6d4
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-27 10:24:24 +1000

    Download image

--- AukTests/AukRemoteImageTests.swift
+++ AukTests/AukRemoteImageTests.swift
@@ -1,19 +1,33 @@
 import UIKit
 import XCTest
+import moa
 
 class AukRemoteImageTests: XCTestCase {
   
   var obj: AukRemoteImage!
-  var imateView: UIImageView!
+  var imageView: UIImageView!
   
   override func setUp() {
     super.setUp()
     
-    imateView = UIImageView()
-    obj = AukRemoteImage(url: "http://site.com/auk.jpg", imageView: imateView)
+    imageView = UIImageView()
+    obj = AukRemoteImage(url: "http://site.com/auk.jpg", imageView: imageView)
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+    
+    MoaSimulator.clear()
   }
   
   func testDownload() {
+    let simulator = MoaSimulator.simulate("auk.jpg")
+    
     obj.downloadImage()
+    
+    let image = uiImageFromFile("96px.png")
+    simulator.respondWithImage(image)
+    
+    XCTAssertEqual(96, imageView.image!.size.width)
   }
 }
