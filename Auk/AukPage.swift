3de70ce29df8098bcc699a45b38d22433e1d8c0e
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-27 12:51:11 +1000

    Comments

--- Auk/AukPage.swift
+++ Auk/AukPage.swift
@@ -3,15 +3,34 @@ import UIKit
 /// The view for an individual page of the scroll view containing an image.
 final class AukPage: UIView {
   
+  // Contain a local image.
   var imageView: UIImageView?
+  
+  // Contains a URL for the remote image, if any.
   var remoteImage: AukRemoteImage?
   
+  /**
+  
+  Shows an image.
+  
+  :param: image: The image to be shown
+  :param: settings: Auk settings.
+  
+  */
   func show(#image: UIImage, settings: AukSettings) {
     createAndLayoutImageView(settings)
     
     imageView?.image = image
   }
   
+  /**
+  
+  Shows a remote image. The image download stars if/when the page becomes visible to the user.
+  
+  :param: url: The URL to the image to be displayed.
+  :param: settings: Auk settings.
+  
+  */
   func show(#url: String, settings: AukSettings) {
     createAndLayoutImageView(settings)
     
@@ -43,6 +62,8 @@ final class AukPage: UIView {
   
   Create and layout an image view.
   
+  :param: settings: Auk settings.
+  
   */
   func createAndLayoutImageView(settings: AukSettings) {
     if imageView != nil { return }
@@ -55,6 +76,13 @@ final class AukPage: UIView {
     layoutImageView(newImageView)
   }
   
+  /**
+  
+  Creates Auto Layout constrains for the image view.
+  
+  :param: imageView: Image view that is used to create Auto Layout constraints.
+  
+  */
   private func layoutImageView(imageView: UIImageView) {
     imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
     
