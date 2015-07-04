3eb0c87a53c584cba42d6aa4b3ec00a7ca7a2428
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-07-04 15:14:18 +1000

    Experimenting

--- Auk/AukAutoscroll.swift
+++ Auk/AukAutoscroll.swift
@@ -6,16 +6,12 @@ Starts and cancels the auto scrolling.
 
 */
 struct AukAutoscroll {
-  var autoscrollTimer: MoaTimer?
+  var autoscrollTimer: AutoCancellingTimer?
   
   mutating func startAutoScroll(scrollView: UIScrollView, delaySeconds: Double,
     forward: Bool, cycle: Bool, animated: Bool, auk: AukInterface) {
       
-    // Cancel the previous timer.
-    // Simply creating another one will NOT cancel the previous timer because the closure will be capturing the timer's instance.
-    autoscrollTimer?.cancel()
-      
-    autoscrollTimer = MoaTimer.runAfter(delaySeconds, repeats: true) { timer in
+    autoscrollTimer = AutoCancellingTimer(interval: delaySeconds, repeats: true) {
       if forward {
         AukScrollTo.scrollToNextPage(scrollView, cycle: cycle,
           animated: animated, currentPageIndex: auk.currentPageIndex,
@@ -29,6 +25,6 @@ struct AukAutoscroll {
   }
   
   mutating func stopAutoScroll() {
-    autoscrollTimer?.cancel()
+    autoscrollTimer = nil // Cancels the timer on deinit
   }
 }
