d7f28fcb302a271f0a93fc96487ce0bc8d7c54de
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-08-02 17:03:48 +1000

    Add right-to-left language support

--- AukTests/Interface/AukInterfaceShowLocalImageTests.swift
+++ AukTests/Interface/AukInterfaceShowLocalImageTests.swift
@@ -85,4 +85,41 @@ class AukInterfaceShowLocalImageTests: XCTestCase {
     XCTAssertEqual(page.accessibilityTraits, UIAccessibilityTraitImage)
     XCTAssert(page.accessibilityLabel == nil)
   }
+  
+  // MARK: - right-to-left language
+  func testShowLocalImage_layoutSubviews_rightToLeft() {
+    if #available(iOS 9.0, *) {
+      scrollView.semanticContentAttribute = UISemanticContentAttribute.ForceRightToLeft
+
+      let image1 = uiImageFromFile("96px.png")
+      auk.show(image: image1)
+      
+      let image2 = uiImageFromFile("67px.png")
+      auk.show(image: image2)
+      
+      scrollView.layoutIfNeeded()
+      
+      // Check content size
+      // -------------
+      
+      XCTAssertEqual(CGSize(width: 240, height: 90), scrollView.contentSize)
+      
+      // View 1
+      // -------------
+      
+      let aukView1 = aukPages(scrollView)[0]
+      XCTAssertEqual(CGPoint(x: 120, y: 0), aukView1.frame.origin)
+      XCTAssertEqual(CGSize(width: 120, height: 90), aukView1.frame.size)
+      XCTAssertEqual(96, firstAukImage(scrollView, pageIndex: 0)!.size.width)
+      
+      // View 2
+      // -------------
+      
+      let aukView2 = aukPages(scrollView)[1]
+      XCTAssertEqual(CGPoint(x: 0, y: 0), aukView2.frame.origin)
+      XCTAssertEqual(CGSize(width: 120, height: 90), aukView2.frame.size)
+      XCTAssertEqual(67, firstAukImage(scrollView, pageIndex: 1)!.size.width)
+
+    }
+  }
 }
