968aa736f4fa165bb66d7c46f426dcb3d3e9004b
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-03-30 08:30:01 +1100

    Simplifying demo app

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -5,24 +5,16 @@ import moa
 class ViewController: UIViewController, UIScrollViewDelegate {
   @IBOutlet weak var scrollView: UIScrollView!
   
-  @IBOutlet weak var contentScrollView: UIScrollView!
-  
   var imageDescriptions = [String]()
   @IBOutlet weak var imageDescriptionLabel: UILabel!
   
   @IBOutlet weak var leftButton: UIButton!
   @IBOutlet weak var rightButton: UIButton!
   @IBOutlet weak var autoScrollButton: UIButton!
-
-  @IBOutlet weak var updateWithLocalButton: UIButton!
-  @IBOutlet weak var updateWithRemoteButton: UIButton!
-  
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    Scrollable.createContentView(contentScrollView)
-        
     layoutButtons()
     
     scrollView.delegate = self
@@ -31,11 +23,80 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 
     showInitialImage()
     showCurrentImageDescription()
-    showOrHideUpdateButtons()
-    showOrHideScrollingButtons()
+  }
+  
+  // Show the first image when the app starts
+  private func showInitialImage() {
+    if let image = UIImage(named: DemoConstants.initialImage.fileName) {
+      scrollView.auk.show(image: image,
+                          accessibilityLabel: DemoConstants.initialImage.description)
+      
+      imageDescriptions.append(DemoConstants.initialImage.description)
+    }
+  }
+
+  // Show local images
+  @IBAction func onShowLocalTapped(sender: AnyObject) {
+    scrollView.auk.stopAutoScroll()
+    for localImage in DemoConstants.localImages {
+      if let image = UIImage(named: localImage.fileName) {
+        scrollView.auk.show(image: image, accessibilityLabel: localImage.description)
+        imageDescriptions.append(localImage.description)
+      }
+    }
     
-    updateWithLocalButton.layer.cornerRadius = 10
-    updateWithRemoteButton.layer.cornerRadius = 10
+    showCurrentImageDescription()
+  }
+
+  // Show remote images
+  @IBAction func onShowRemoteTapped(sender: AnyObject) {
+    scrollView.auk.stopAutoScroll()
+    for remoteImage in DemoConstants.remoteImages {
+      let url =  "\(DemoConstants.remoteImageBaseUrl)\(remoteImage.fileName)"
+      scrollView.auk.show(url: url, accessibilityLabel: remoteImage.description)
+      
+      imageDescriptions.append(remoteImage.description)
+    }
+    
+    showCurrentImageDescription()
+  }
+  
+  // Scroll to the next image
+  @IBAction func onShowRightButtonTapped(sender: AnyObject) {
+    scrollView.auk.stopAutoScroll()
+    
+    if RightToLeft.isRightToLeft(view) {
+      scrollView.auk.scrollToPreviousPage()
+    } else {
+      scrollView.auk.scrollToNextPage()
+    }
+  }
+
+  // Scroll to the previous image
+  @IBAction func onShowLeftButtonTapped(sender: AnyObject) {
+    scrollView.auk.stopAutoScroll()
+    
+    if RightToLeft.isRightToLeft(view) {
+      scrollView.auk.scrollToNextPage()
+    } else {
+      scrollView.auk.scrollToPreviousPage()
+    }
+  }
+
+  // Remove all images
+  @IBAction func onDeleteButtonTapped(sender: AnyObject) {
+    scrollView.auk.stopAutoScroll()
+    scrollView.auk.removeAll()
+    imageDescriptions = []
+    showCurrentImageDescription()
+  }
+
+  @IBAction func onAutoscrollTapped(sender: AnyObject) {
+    scrollView.auk.startAutoScroll(delaySeconds: 2)
+  }
+  
+  @IBAction func onScrollViewTapped(sender: AnyObject) {
+    imageDescriptionLabel.text = "Tapped image #\(scrollView.auk.currentPageIndex)"
   }
   
   private func layoutButtons() {
@@ -43,12 +104,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     layoutButtons(autoScrollButton, secondView: rightButton)
   }
   
-  private func showOrHideScrollingButtons() {
-    leftButton.hidden = scrollView.auk.numberOfPages == 0
-    rightButton.hidden = scrollView.auk.numberOfPages == 0
-    autoScrollButton.hidden = scrollView.auk.numberOfPages == 0
-  }
-  
+  // Use left/right constraints instead of leading/trailing to prevent buttons from changing their place for right-to-left languages.
   private func layoutButtons(firstView: UIView, secondView: UIView) {
     let constraint = NSLayoutConstraint(
       item: secondView,
@@ -62,25 +118,27 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     view.addConstraint(constraint)
   }
   
+  // MARK: - Handle orientation change
+  
   /// Animate scroll view on orientation change
   override func viewWillTransitionToSize(size: CGSize,
-    withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
-
+                                         withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+    
     super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
-
+    
     guard let pageIndex = scrollView.auk.currentPageIndex else { return }
     let newScrollViewWidth = size.width // Assuming scroll view occupies 100% of the screen width
-
+    
     coordinator.animateAlongsideTransition({ [weak self] _ in
       self?.scrollView.auk.scrollTo(pageIndex, pageWidth: newScrollViewWidth, animated: false)
-    }, completion: nil)
+      }, completion: nil)
   }
   
   /// Animate scroll view on orientation change
   /// Support iOS 7 and older
   override func willRotateToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation,
-    duration: NSTimeInterval) {
-      
+                                                 duration: NSTimeInterval) {
+    
     super.willRotateToInterfaceOrientation(toInterfaceOrientation, duration: duration)
     
     var screenWidth = UIScreen.mainScreen().bounds.height
@@ -91,107 +149,8 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     guard let pageIndex = scrollView.auk.currentPageIndex else { return }
     scrollView.auk.scrollTo(pageIndex, pageWidth: screenWidth, animated: false)
   }
-
-  @IBAction func onShowLocalTapped(sender: AnyObject) {
-    scrollView.auk.stopAutoScroll()
-    for localImage in DemoConstants.localImages {
-      if let image = UIImage(named: localImage.fileName) {
-        scrollView.auk.show(image: image, accessibilityLabel: localImage.description)
-        imageDescriptions.append(localImage.description)
-      }
-    }
-    
-    showCurrentImageDescription()
-    showOrHideUpdateButtons()
-    showOrHideScrollingButtons()
-  }
-
-  @IBAction func onShowRemoteTapped(sender: AnyObject) {
-    scrollView.auk.stopAutoScroll()
-    for remoteImage in DemoConstants.remoteImages {
-      let url = remoteImageUrl(remoteImage.fileName)
-      scrollView.auk.show(url: url, accessibilityLabel: remoteImage.description)
-      
-      imageDescriptions.append(remoteImage.description)
-    }
-    
-    showCurrentImageDescription()
-    showOrHideUpdateButtons()
-    showOrHideScrollingButtons()
-  }
   
-  private func remoteImageUrl(fileName: String) -> String {
-    return "\(DemoConstants.remoteImageBaseUrl)\(fileName)"
-  }
-  
-  @IBAction func didTapUpdateCurrentImageLocal(sender: AnyObject) {
-    guard let localImage = DemoConstants.localImages.last else { return }
-    guard let image = UIImage(named: localImage.fileName) else { return }
-    guard let currentPageIndex = scrollView.auk.currentPageIndex else { return }
-    
-    scrollView.auk.updateAt(currentPageIndex, image: image)
-    changeCurrentImageDescription(localImage.description)
-  }
-
-  @IBAction func didTapUpdateCurrentImageRemote(sender: AnyObject) {
-    guard let remoteImage = DemoConstants.remoteImages.first else { return }
-    guard let currentPageIndex = scrollView.auk.currentPageIndex else { return }
-
-    let url = remoteImageUrl(remoteImage.fileName)
-    scrollView.auk.updateAt(currentPageIndex, url: url)
-    changeCurrentImageDescription(remoteImage.description)
-  }
-  
-  private func showOrHideUpdateButtons() {
-    updateWithLocalButton.hidden = scrollView.auk.numberOfPages == 0
-    updateWithRemoteButton.hidden = scrollView.auk.numberOfPages == 0
-  }
-  
-  @IBAction func onShowRightButtonTapped(sender: AnyObject) {
-    scrollView.auk.stopAutoScroll()
-    
-    if RightToLeft.isRightToLeft(view) {
-      scrollView.auk.scrollToPreviousPage()
-    } else {
-      scrollView.auk.scrollToNextPage()
-    }
-  }
-
-  @IBAction func onShowLeftButtonTapped(sender: AnyObject) {
-    scrollView.auk.stopAutoScroll()
-    
-    if RightToLeft.isRightToLeft(view) {
-      scrollView.auk.scrollToNextPage()
-    } else {
-      scrollView.auk.scrollToPreviousPage()
-    }
-  }
-
-  @IBAction func onDeleteButtonTapped(sender: AnyObject) {
-    scrollView.auk.stopAutoScroll()
-    scrollView.auk.removeAll()
-    imageDescriptions = []
-    showCurrentImageDescription()
-    showOrHideUpdateButtons()
-    showOrHideScrollingButtons()
-  }
-
-  @IBAction func onAutoscrollTapped(sender: AnyObject) {
-    scrollView.auk.startAutoScroll(delaySeconds: 2)
-  }
-  
-  @IBAction func onScrollViewTapped(sender: AnyObject) {
-    imageDescriptionLabel.text = "Tapped image #\(scrollView.auk.currentPageIndex)"
-  }
-
-  private func showInitialImage() {
-    if let image = UIImage(named: DemoConstants.initialImage.fileName) {
-      scrollView.auk.show(image: image,
-        accessibilityLabel: DemoConstants.initialImage.description)
-      
-      imageDescriptions.append(DemoConstants.initialImage.description)
-    }
-  }
+  // MARK: - Image description
   
   private func showCurrentImageDescription() {
     if let description = currentImageDescription {
