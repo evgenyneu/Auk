28ea0c47bb1d4fd06ad5c5826367c185889cb86e
Valentin Pertuisot <valentin.pertuisot@gmail.com>
2016-07-04 12:01:38 +0100

    Page removal demo

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -91,6 +91,20 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     showCurrentImageDescription()
   }
 
+  @IBAction func onDeleteCurrentButtonTapped(sender: AnyObject) {
+    guard let indexToRemove = scrollView.auk.currentPageIndex else {
+      return
+    }
+    scrollView.auk.stopAutoScroll()
+    
+    if scrollView.auk.removeCurrentPage(animated: true) {
+      if imageDescriptions.count >= scrollView.auk.numberOfPages {
+        imageDescriptions.removeAtIndex(indexToRemove)
+      }
+      showCurrentImageDescription()
+    }
+  }
+  
   @IBAction func onAutoscrollTapped(sender: AnyObject) {
     scrollView.auk.startAutoScroll(delaySeconds: 2)
   }
